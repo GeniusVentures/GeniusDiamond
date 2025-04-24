a7cc252e80fec6a21804d534e996f56374539751
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2025-04-24 11:04:28 -0400

    added test deplyoments for sepolia and polygon_amoy

--- callbacks/GNUSControl.ts
+++ callbacks/GNUSControl.ts
@@ -10,26 +10,26 @@ import util from "util";
 export async function registerProtocolVersionChainId(callbackArgs: CallbackArgs) {
   console.log("Starting ERC1155ProxyOperator callback registerProtocolVersionChainId");
 
-  const { diamond } = callbackArgs;
+  // const { diamond } = callbackArgs;
 
-  const chainID = diamond.chainId;
-  const diamondName = diamond.diamondName;
-  const networkName = diamond.networkName;
-  const deployInfo = diamond.getDeployedDiamondData();
-  const deployer = diamond.signer!;
+  // const chainID = diamond.chainId;
+  // const diamondName = diamond.diamondName;
+  // const networkName = diamond.networkName;
+  // const deployInfo = diamond.getDeployedDiamondData();
+  // const deployer = diamond.signer!;
 
-  debuglog('UnitTest:log', `In GNUSControl callback function for networkName: ${networkName}  chainID: ${chainID}`);
+  // debuglog('UnitTest:log', `In GNUSControl callback function for networkName: ${networkName}  chainID: ${chainID}`);
 
-  const diamondAddress = diamond.getDeployedDiamondData().DiamondAddress!;
-  const diamondArtifactName = `hardhat-diamond-abi/HardhatDiamondABI.sol:${diamondName}`;
-  const diamondArtifact = hre.artifacts.readArtifactSync(diamondArtifactName);
-  const diamondContract = new hre.ethers.Contract(diamondAddress, diamondArtifact.abi, diamond.provider) as GeniusDiamond;
-  const deployerDiamondContract = diamondContract.connect(deployer);
+  // const diamondAddress = diamond.getDeployedDiamondData().DiamondAddress!;
+  // const diamondArtifactName = `hardhat-diamond-abi/HardhatDiamondABI.sol:${diamondName}`;
+  // const diamondArtifact = hre.artifacts.readArtifactSync(diamondArtifactName);
+  // const diamondContract = new hre.ethers.Contract(diamondAddress, diamondArtifact.abi, diamond.provider) as GeniusDiamond;
+  // const deployerDiamondContract = diamondContract.connect(deployer);
 
-  deployerDiamondContract.setChainID(chainID);
-  const protocolVersion = deployInfo.protocolVersion || 0.0;
-  deployerDiamondContract.setProtocolVersion(Math.round(protocolVersion * 100));
+  // deployerDiamondContract.setChainID(chainID);
+  // const protocolVersion = deployInfo.protocolVersion || 0.0;
+  // deployerDiamondContract.setProtocolVersion(Math.round(protocolVersion * 100));
 
-  const info = diamondContract.protocolInfo();
-  debuglog(`protocol info: \n${util.inspect(info)}`)
+  // const info = diamondContract.protocolInfo();
+  // debuglog(`protocol info: \n${util.inspect(info)}`)
 }
